5738891b458cb67da7292b17251a373b8731e6a7
ritijjain <ritij@ritijjain.com>
2022-10-13 00:07:45 -0400

    Vanila deploy config

--- chot_api/chot_api/settings.py
+++ chot_api/chot_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ioytb%4&#f$9qa%t-19_v^pxh6yekfp_((=q_(nz-c$3@a4okj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,13 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-if bool(int(os.getenv('HEROKU', 0))) == True:
-    import django_heroku
-    django_heroku.settings(locals(), databases=False)
